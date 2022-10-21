8b4b1c043fbb4ee313642911d66ed87dce52b099
Perseverance Mbewe <111703329+pnmbewe@users.noreply.github.com>
2022-10-21 13:40:25 +0200

    added function that creates createHeirarchy

--- main.js
+++ main.js
@@ -108,3 +108,59 @@ console.info('Your application must have the following output:\n');
 console.info('* Terry Cats - Chief Creative Officer: Executive\n\t* Nick Thompson - Head of Design: Operations\n\t\t * Nick Jenson - Intern designer: Interns\n* Bruce Davids - Chief Strategy Officer: Executive\n\t* David Smith - Head of Development: Operations\n\t* John Jones - Head of Marketing: Operations\n\t* Jane Sampson - Head of Content: Operations\n\t* Simon Says - Head of Strategy: Operations\n* Bill Bass - Chief Executive Officer: Executive');
 
 // Start your code here but please comment out the above logs
+const createHeirarchy = (staffMembers, categories) => {
+    // get head of staff members
+    const staffHead = [];
+    for (let i = 0; i < staffMembers.length; i++) {
+        // if the staff member reports to no one
+        if (staffMembers[i].reportsTo === '') {
+            staffHead.push(staffMembers[i]);
+        }
+        // if the staff member reports to someone do nothing
+    }
+
+
+    // create an empty string to store the result
+    let result = ``;
+    // loop through the head members
+    staffHead.map(e => {
+        // find the category of the head member
+        const category = categories.find(c => c.slug === e.category);
+        // if the result string is empty
+        if (result.length === 0) {
+            // add the head member to the result string
+            result += `* ${e.name} ${e.surname} - ${e.title}: ${category.name}`;
+        } else {
+            // else add the head member to the result string
+            result += `\n* ${e.name} ${e.surname} - ${e.title}: ${category.name}`;
+        }
+        // loop through the staff members
+        staffMembers.map(a => {
+            // if the staff member reports to the head member
+            if (a.reportsTo === e.slug) {
+                // find the category of the staff member
+                const category2 = categories.find(c => c.slug === a.category);
+                // add the staff member to the result string
+                result += `\n\t* ${a.name} ${a.surname} - ${a.title}: ${category2.name}`;
+                // loop through the staff members
+                staffMembers.map(d => {
+                    // if the staff member reports to the staff member
+                    if (d.reportsTo === a.slug) {
+                        // find the category of the staff member
+                        const category3 = categories.find(c => c.slug === d.category);
+                        // add the staff member to the result string
+
+                        result += `\n\t\t* ${d.name} ${d.surname} - ${d.title}: ${category3.name}`;
+                    }
+                });
+            }
+        });
+    })
+    // return the result string
+    return result;
+}
+
+
+console.log(createHeirarchy(staffMembers, categories));
+
+
